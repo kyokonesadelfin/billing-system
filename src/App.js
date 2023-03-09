2cb9ef9927f0af19b85a36a8d249f7de921d5dd0
unknown <jo-lee@global-mobility-service.com>
2023-03-09 17:34:41 +0800

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.scss';
 import { Route, Routes  } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
 import Billing from './pages/Billing';
 import LandingPage from './landing_page/LandingPage';
+import LoginForm from './login_page/LoginForm';
+import NotFound from './pages/NotFound';
+import Dashboard from './pages/Dashboard';
 
 
 function App() {
   return (
     <>
       <Routes>
-        <Route exact path='/dashboard' element={<Dashboard/>}/>
         <Route exact path='/billing' element={<Billing/>}/>
         <Route exact path='/' element={<LandingPage/>}/>
+        <Route exact path='/login' element={<LoginForm/>}/>
+        <Route path='*' element={<NotFound/>} />
+        <Route path='/dashboard' element={<Dashboard/>} />
       </Routes>
     </>
   );

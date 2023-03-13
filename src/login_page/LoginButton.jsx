a1527c84a9e1a47f969158b0eb90ea4d6bd383a0
Kyokonesa Delfin <ky-delfin@global-mobility-service.com>
2023-03-13 16:12:54 +0800

    added profile menu on the dashboard

--- src/login_page/LoginButton.jsx
+++ src/login_page/LoginButton.jsx
@@ -37,7 +37,7 @@ function LoginButton() {
     <div>
       <GoogleLogin
         clientId={clientId}
-        buttonText="Google Login"
+        buttonText="Sign in with Google"
         onSuccess={onSuccess}
         onFailure={onFailure}
         cookiePolicy={'none'}
--- src/pages/Billing.jsx
+++ src/pages/Billing.jsx
@@ -9,11 +9,6 @@ import { Modal } from 'bootstrap';
 import moment from 'moment';
 
 function Billing() {
-    const [marginLeft, setMarginLeft] = useState(50);
-    const [width, setWidth] = useState(50);
-    const [dropdownActive1, setDropdownActive1] = useState(false);
-    const [dropdownActive2, setDropdownActive2] = useState(false);
-    const [dropdownActive3, setDropdownActive3] = useState(false);
     const [device, setDevice] = useState([]);
     const [loading, setLoading] = useState(false);
     const [dataPerPage] = useState(10);
@@ -44,27 +39,6 @@ function Billing() {
     // Counts the filtered search results that will be displayed under the data table
     const searchCount = Math.ceil(combinedArrays.length);
 
-    const toggleDropdown1 = () => {
-        setDropdownActive1(!dropdownActive1);
-    };
-
-    const toggleDropdown2 = () => {
-        setDropdownActive2(!dropdownActive2);
-    };
-
-    const toggleDropdown3 = () => {
-        setDropdownActive3(!dropdownActive3);
-    };
-
-    const toggleNav = () => {
-        if (width === 250 && marginLeft === 250) {
-            setWidth(50);
-            setMarginLeft(50);
-        } else {
-            setWidth(250);
-            setMarginLeft(250);
-        }
-    }
 
     // Search function using Device Id or Vehicle Name(VIN)
     function searchFilter(e) {
@@ -205,156 +179,7 @@ function Billing() {
     }
 
     return (
-        <div>
-            <div className="icon-bar" id="sidenav" style={{ width: `${width}px` }}>
-                {width === 50 ? (
-                    <>
-                        <div style={{ marginTop: "42px" }}>
-                            <a href="/#">
-                                <i className="fa fa-home"></i>
-                            </a>
-                            <button
-                                className={dropdownActive1 ? 'dropdown-btn active' : 'dropdown-btn'}
-                                onClick={toggleDropdown1}
-                            >
-                                <i className="fa fa-user-times"></i>
-                            </button>
-                            <div className="dropdown-container" style={{ display: dropdownActive1 ? 'block' : 'none' }}>
-                                <a href="/#" id='sidenav-dropdown'>Non Graduates</a>
-                            </div>
-                            <button
-                                className={dropdownActive2 ? 'dropdown-btn active' : 'dropdown-btn'}
-                                onClick={toggleDropdown2}
-                            >
-                                <i className="fa fa-list-alt"></i>
-                            </button>
-                            <div className="dropdown-container" style={{ display: dropdownActive2 ? 'block' : 'none' }}>
-                                <a href="/billing" id='sidenav-dropdown'>Vehicles</a>
-                            </div>
-
-                            <button
-                                className={dropdownActive3 ? 'dropdown-btn active' : 'dropdown-btn'}
-                                onClick={toggleDropdown3}
-                            >
-                                <i className="fa fa-map-o"></i>
-                            </button>
-                            <div className="dropdown-container" style={{ display: dropdownActive3 ? 'block' : 'none' }}>
-                                <a href="/billing" id='sidenav-dropdown'>Vehicle</a>
-                                <a href="/#" id='sidenav-dropdown'>Organization</a>
-                                <a href="/#" id='sidenav-dropdown'>TODA</a>
-                                <a href="/#" id='sidenav-dropdown'>Sales Person</a>
-                                <a href="/#" id='sidenav-dropdown'>Guarantor</a>
-                                <a href="/#" id='sidenav-dropdown'>Employees</a>
-                            </div>
-
-                            <a href="/#">
-                                <i className="fa fa-user-plus"></i>
-                            </a>
-                            <a href="/#">
-                                <i className="fa fa-newspaper-o"></i>
-                            </a>
-                            <a href="/#">
-                                <i className="fa fa-user-secret"></i>
-                            </a>
-                            <a href="/#">
-                                <i className="fa fa-sign-out">
-                                </i>
-                            </a>
-                        </div>
-
-                    </>
-                ) : null}
-                {
-                    width !== 50 ? (
-                        <>
-                            <div style={{ marginTop: "42px" }}>
-                                <a href="/#" id='expanded-side-menu'>
-                                    <i className="fa fa-home">
-                                        <span id='expanded-menu-list' >Dashboard</span>
-                                    </i>
-                                </a>
-                                <button
-                                    className={dropdownActive1 ? 'dropdown-btn active' : 'dropdown-btn'}
-                                    onClick={toggleDropdown1}
-                                    id='expanded-side-menu'
-                                >
-                                    <i className="fa fa-user-times"><span id='expanded-menu-list'>Client Accounts</span></i><i className="fa fa-caret-down mt-2"></i>
-                                </button>
-                                <div className="dropdown-container" style={{ display: dropdownActive1 ? 'block' : 'none' }}>
-                                    <a href="/#" id='sidenav-dropdown'>Non Graduates</a>
-                                </div>
-
-                                <button
-                                    className={dropdownActive2 ? 'dropdown-btn active' : 'dropdown-btn'}
-                                    onClick={toggleDropdown2}
-                                    id='expanded-side-menu'
-                                >
-                                    <i className="fa fa-list-alt"><span id='expanded-menu-list'>Billing Operations</span></i><i className="fa fa-caret-down mt-2"></i>
-                                </button>
-                                <div className="dropdown-container" style={{ display: dropdownActive2 ? 'block' : 'none' }}>
-                                    <a href="/billing" id='sidenav-dropdown'>Vehicles</a>
-                                </div>
-
-                                <button
-                                    className={dropdownActive3 ? 'dropdown-btn active' : 'dropdown-btn'}
-                                    onClick={toggleDropdown3}
-                                    id='expanded-side-menu'
-                                >
-                                    <i className="fa fa-map-o"><span id='expanded-menu-list'>Catalog</span></i><i className="fa fa-caret-down mt-2"></i>
-                                </button>
-                                <div className="dropdown-container" style={{ display: dropdownActive3 ? 'block' : 'none' }}>
-                                    <a href="/billing" id='sidenav-dropdown'>Vehicle</a>
-                                    <a href="/#" id='sidenav-dropdown'>Organization</a>
-                                    <a href="/#" id='sidenav-dropdown'>TODA</a>
-                                    <a href="/#" id='sidenav-dropdown'>Sales Person</a>
-                                    <a href="/#" id='sidenav-dropdown'>Guarantor</a>
-                                    <a href="/#" id='sidenav-dropdown'>Employees</a>
-                                </div>
-
-                                <a href="/#" id='expanded-side-menu'>
-                                    <i className="fa fa-user-plus me-2">
-                                        <span id='expanded-menu-list'>Account Management</span>
-                                    </i>
-                                </a>
-                                <a href="/#" id='expanded-side-menu'>
-                                    <i className="fa fa-newspaper-o me-2">
-                                        <span id='expanded-menu-list'>Reports</span>
-                                    </i>
-                                </a>
-                                <a href="/#" id='expanded-side-menu'>
-                                    <i className="fa fa-user-secret me-2">
-                                        <span id='expanded-menu-list'>Admin Management</span>
-                                    </i>
-                                </a>
-                                <div>
-                                    <a href="/#" id='expanded-side-menu'>
-                                        <i className="fa fa-sign-out me-2">
-                                            <span id='expanded-menu-list'>Log out</span>
-                                        </i>
-                                    </a>
-                                </div>
-                            </div>
-
-
-                        </>
-                    ) : null
-                }
-            </div>
-            <div class="topnav" style={{ marginLeft: `${marginLeft}px` }}>
-                <span
-                    style={{
-                        fontSize: '20px',
-                        cursor: 'pointer',
-                        color: 'white',
-                        padding: '10px 20px 0px 0px',
-                    }}
-                    onClick={toggleNav}
-                >
-                    {width === 65 ? '\u2630' : '\u2630'}
-                </span>
-                <img src='/assets/gms_logo_w_r.svg' alt='gms-logo' width='140' height='50' />
-            </div>
-            <div id="main" style={{ marginLeft: `${marginLeft}px` }}>
+        <div >
                 {
                     loading
                         ?
@@ -603,7 +428,6 @@ function Billing() {
                 }
 
             </div>
-        </div>
     )
 
 }
